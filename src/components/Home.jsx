36feac6877937d8afb333def652b3a1c88ed0395
Anara Akmatalieva <anara.bakyt@gmail.com>
2022-07-14 19:41:04 +0600

    added viewers

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import ImgSlider from './ImgSlider'
+import Viewers from './Viewers'
 const Home = () => {
   return (
     <Container>
@@ -32,7 +33,7 @@ overflow-x:hidden;
 }
 `
 
-const Viewers=styled.div`
-`
+
+
 const Movies=styled.div`
 `
